a83c5bd2fbe66a29c044be0ec0bf25f54be9731a
omkar4771 <omkardaswadkar123@gmail.com>
2024-06-10 23:08:19 +0530

    index file

---
+++ index.js
@@ -0,0 +1,11 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().then(()=>{
+    console.log("connection sucessfull");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
