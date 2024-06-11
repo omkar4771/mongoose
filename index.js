dd641eb9fbb7d9d350ffea7e3926453443c49596
Omkar Daswadkar <158741267+omkar4771@users.noreply.github.com>
2024-06-11 23:44:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ main().then(()=>{
     console.log("connection sucessfull");
 })
 .catch(err => console.log(err));
-
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
